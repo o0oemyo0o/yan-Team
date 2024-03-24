a9eb1382bb1fd86dc2020f12922d7b8d53837fc3
Emy Al Ahmad <78692236+o0oemyo0o@users.noreply.github.com>
2024-03-24 21:02:00 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,15 +4,5 @@ function login_Nafath(){
 window.open("https://www.iam.gov.sa/authservice/userauthservice?lang=ar");
 }
 
-/*الوصول للدواء*/
-let c1=document.getElementsByName('.c1');
-let c2=document.querySelector('.c2');
-let c3=document.querySelector('.c3');
 
-function open_med(){
-    window.open('medicine1.html'); 
-}
-c1.addEventListener("click", open_med);
-c2.addEventListener("click", open_med);
-c3.addEventListener("click", open_med);
 
